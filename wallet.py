e9a6b5bec902fa673da9daa09dfb2f7402824d47
plumtree87 <plumtree87@protonmail.com>
2021-03-25 20:58:33 +0500

    Fixed like 3 errors, right now I'm looking at the fill_wallet() function in Wallet class, trying to figure out where in the world it's suppose to be used...

---
+++ wallet.py
@@ -0,0 +1,17 @@
+from coins import Coin, Quarter, Dime, Nickel, Penny
+
+class Wallet:
+    def __init__(self):
+        self.money = []
+
+
+    def fill_wallet(self):
+        """Method will fill wallet's money list with certain amount of each type of coin when called."""
+        for index in range(8):
+            self.money.append(Quarter())
+        for index in range(10):
+            self.money.append(Dime())
+        for index in range(20):
+            self.money.append(Nickel())
+        for index in range(50):
+            self.money.append(Penny())
